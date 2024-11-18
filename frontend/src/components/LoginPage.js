ec6ce10838e480fe309ee59d1a5abf57b1ef598f
rimel33 <101441759+rimel33@users.noreply.github.com>
2024-11-18 12:47:16 -0500

    Create LoginPage.js

---
+++ frontend/src/components/LoginPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../LoginFormComponent.js"; // login form
+
+const LoginPage = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
